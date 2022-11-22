586967b6c4a216c7c6fd3ef00f1416430f71d34f
adnanali-in <adnanali.in@gmail.com>
2022-11-22 10:32:45 +0530

    Basics of JS

---
+++ basics/basics1.js
@@ -0,0 +1,45 @@
+//comment
+//console.log("") this is used to print output
+//semi colon ; is not necessary in javascript
+ 
+console.log("Hello boi")
+
+
+//cosole log adds a new line by default at the end
+//here "," is used to add seperate things in console.log log
+//to print """ use "/"" now the output will be """
+//to print "/" use "//" now the output will be "/"
+
+
+console.log("Syed Anwar","rizwan")
+
+/*
+Welcome to JS!
+Adnan "ali" Khan
+5/10=0.5
+This is 'JS'
+Hi, How are you?
+:\\\
+
+*/
+
+
+console.log("Welcome to JS!")
+console.log("Adnan \"ali\" Khan")
+console.log("5/10= 0.5")
+console.log("This is 'JS'")
+console.log("Hi,How are you?")
+console.log(":\\\\\\")
+
+
+/*
+p
+*/
+
+console.log("Adnan \"ali\" khan");
+console.log('This is \'JS\'');
+console.log(":\\\\\\");
+
+// \n for New Line
+console.log(`Adnan Ali \nKhan`);
+console.log(`\tThis is a Tab Space`);
